58acc4b0d7c3462fcd99617c0d5938bf03979c11
manoj1919 <manoj.mystyle@gmail.com>
2021-06-21 11:16:28 +0530

    fix pcap file naming

--- free5gc-compose/config/pcap.sh
+++ free5gc-compose/config/pcap.sh
@@ -2,5 +2,5 @@
     
   ### run tshark to capture packets within upf
   ### goto 5-fi-packetdata folder and run tail -c +1 -f upf-1.pcap | wireshark -k -i - to view the traffic live
-  UPFNS="upf-1"
-  tcpdump -i any -w ../pcapdata/${UPFNS}.pcap
+  NF=$1
+  tcpdump -i any -w ../pcapdata/${NF}.pcap
