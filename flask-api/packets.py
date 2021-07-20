d5c0bb47809385ae8ec3a4100ad85657a932502f
Manoj <manoj.mystyle@gmail.com>
2021-07-20 19:03:14 +0530

    minor

--- flask-api/packets.py
+++ flask-api/packets.py
@@ -1,7 +1,7 @@
 import os
 import json
 def get_packets(name):
-    cmd= 'tshark -r ~/5-fi-packetdata/'+ name+'.pcap -Y " ngap || nas-5gs || pfcp || icmp || gtp" -e _ws.col.No. -e frame.time -e ip.src -e ip.dst -e _ws.col.Protocol -e _ws.col.Info -l -n -T json -c 20000'
+    cmd= 'tshark -r ~/5-fi-packetdata/'+ name+'.pcap -Y " ngap || nas-5gs || pfcp || icmp || gtp" -e _ws.col.No. -e frame.time -e ip.src -e ip.dst -e _ws.col.Protocol -e _ws.col.Info -l -n -T json'
     if 'gnb' in name:
         cmd= 'tshark -r ~/5-fi-packetdata/'+ name+'.pcap -Y " ngap || nas-5gs || pfcp || icmp " -e _ws.col.No. -e frame.time -e ip.src -e ip.dst -e _ws.col.Protocol -e _ws.col.Info -l -n -T json -c 20000'
     stream=os.popen(cmd)
