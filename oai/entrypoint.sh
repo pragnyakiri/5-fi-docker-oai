8f1f6529cc7157d45f497aa6ac16c214724e5df6
pragnyakiri <pradgnya.kiri@dolcera.com>
2022-02-21 16:53:55 +0530

    entrypoint

--- oai/entrypoint.sh
+++ oai/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export AMF_ADDR="$(host -4 amf |awk '/has.*address/{print $NF; exit}')" 
-sed -i "s/CI_MME_IP_ADDR/${AMF_ADDR}/" /opt/oai-gnb/etc/gnb.conf
+sed -i "s/CI_MME_IP_ADDR/$AMF_ADDR/" /opt/oai-gnb/etc/gnb.conf
 sed -i 's/CI_GNB_IP_ADDR/10.100.200.50/' /opt/oai-gnb/etc/gnb.conf
 sed -i 's/em1/eth0/' /opt/oai-gnb/etc/gnb.conf
